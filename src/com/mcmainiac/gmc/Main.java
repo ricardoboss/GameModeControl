8c62748bc7b5d24ffd958ef9d6cd66cbd7e7d0f1
Ricardo Boss <ricardo.boss@web.de>
2015-08-20 15:54:20 +0200

    Moved commands into their own class and added /gmtemp command
    /gmtemp <player> <game mode> <seconds>

--- src/com/mcmainiac/gmc/Main.java
+++ src/com/mcmainiac/gmc/Main.java
@@ -8,9 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,12 +16,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.google.common.collect.ImmutableMap;
 import com.mcmainiac.gmc.excpetions.GameModeNotFoundException;
 import com.mcmainiac.gmc.excpetions.PlayerNotFoundException;
-import com.mcmainiac.gmc.helpers.CGM;
-import com.mcmainiac.gmc.helpers.CGM.ControlledGameMode;
+import com.mcmainiac.gmc.helpers.Commands;
 import com.mcmainiac.gmc.helpers.Config;
-import com.mcmainiac.gmc.helpers.Help;
-import com.mcmainiac.gmc.helpers.MessageColor;
 import com.mcmainiac.gmc.helpers.Updater;
+import com.mcmainiac.gmc.utils.MessageColor;
 
 /**
  * GameModeControl V1.3.3
@@ -35,14 +31,12 @@ import com.mcmainiac.gmc.helpers.Updater;
  * {@link} http://bit.ly/MC-GMC
  * @author MCMainiac
  */
-public class Main extends JavaPlugin implements Listener {
-	private static final String pre = "§7[GMC] §r";
-	private static CommandSender console = Bukkit.getConsoleSender();
-	
-	private HashMap<String, Permission> permissions = new HashMap<String, Permission>(); // A map for all the permissions (fast access to dynamic permissions)
-	private HashMap<Player, Boolean[]> otgm = new HashMap<Player, Boolean[]>(); // A map to save, which players are able to change their game mode
-	
+public class Main extends JavaPlugin {
+	public static final String pre = "§7[GMC] §r";
+	public static HashMap<String, Permission> permissions = new HashMap<String, Permission>(); // A map for all the permissions (fast access to dynamic permissions)
 	public static Config config;
+
+	private static CommandSender console = Bukkit.getConsoleSender();
 
 	//--------------
 	// Plugin commands
@@ -52,7 +46,7 @@ public class Main extends JavaPlugin implements Listener {
 		try {
 			log("Initializing GMC v" + this.getDescription().getVersion());
 			Main.config = new Config(this);
-			Bukkit.getPluginManager().registerEvents(this, this);
+			Bukkit.getPluginManager().registerEvents(((Listener)new Commands(this)), this);
 			
 			// Permissions
 			permissions.put("gmi", new Permission("gmc.gmi", PermissionDefault.TRUE));
@@ -60,6 +54,7 @@ public class Main extends JavaPlugin implements Listener {
 			permissions.put("forcegm", new Permission("gmc.forcegm", PermissionDefault.TRUE));
 			permissions.put("gmr", new Permission("gmc.gmr", PermissionDefault.OP));
 			permissions.put("gmonce", new Permission("gmc.gmonce", PermissionDefault.OP));
+			permissions.put("gmtemp", new Permission("gmc.gmtemp", PermissionDefault.OP));
 			
 			permissions.put("gm", new Permission("gmc.gm", PermissionDefault.OP));
 			permissions.put("gm.self", new Permission("gmc.gm.self", PermissionDefault.OP));
@@ -80,9 +75,7 @@ public class Main extends JavaPlugin implements Listener {
 			permissions.put("spectator.self", new Permission("gmc.spectator.self", PermissionDefault.OP));
 			permissions.put("spectator.others", new Permission("gmc.spectator.others", PermissionDefault.OP));
 			
-			for (Player p : Bukkit.getOnlinePlayers()) { // if the server has been reloaded, this loop adds all players to the change-gm-one-time-map
-				otgm.put(p, new Boolean[]{false, false, false, false}); // default is false for every game mode
-			}
+			new Commands(this);
 			
 			// Auto-Updater
 			checkForUpdates(config.getBoolean("options.auto-update"));
@@ -106,26 +99,28 @@ public class Main extends JavaPlugin implements Listener {
 			switch (commandLabel.toLowerCase()) {
 			case "gamemode":
 			case "gm":
-				return (args.length > 0 ? Gamemode(sender, new String[]{args[0], (args.length > 1 ? args[1] : null), "gamemode"}) : false);
+				return (args.length > 0 ? Commands.Gamemode(sender, new String[]{args[0], (args.length > 1 ? args[1] : null), "gamemode"}) : false);
 			case "gm0":
 			case "survival":
-				return Gamemode(sender, new String[]{"0", (args.length > 0 ? args[0] : null), "survival"});
+				return Commands.Gamemode(sender, new String[]{"0", (args.length > 0 ? args[0] : null), "survival"});
 			case "gm1":
 			case "creative":
-				return Gamemode(sender, new String[]{"1", (args.length > 0 ? args[0] : null), "creative"});
+				return Commands.Gamemode(sender, new String[]{"1", (args.length > 0 ? args[0] : null), "creative"});
 			case "gm2":
 			case "adventure":
-				return Gamemode(sender, new String[]{"2", (args.length > 0 ? args[0] : null), "adventure"});
+				return Commands.Gamemode(sender, new String[]{"2", (args.length > 0 ? args[0] : null), "adventure"});
 			case "gm3":
 			case "spectator":
-				return Gamemode(sender, new String[]{"3", (args.length > 0 ? args[0] : null), "spectator"});
+				return Commands.Gamemode(sender, new String[]{"3", (args.length > 0 ? args[0] : null), "spectator"});
 			
 			case "gmonce":
-				return OneTimeGamemode(sender, args);
+				return Commands.OneTimeGamemode(sender, args);
+			case "gmtemp":
+				return Commands.TemporaryGamemode(sender, args);
 			
-			case "gmh": return Help(sender, args);
-			case "gmi": return Info(sender, args);
-			case "gmr": return Reload(sender);
+			case "gmh": return Commands.Help(sender, args);
+			case "gmi": return Commands.Info(sender, args);
+			case "gmr": return Commands.Reload(sender);
 			default: return false;
 			}
 		} catch (InvalidParameterException ipe) {
@@ -136,156 +131,6 @@ public class Main extends JavaPlugin implements Listener {
 			gme.printStackTrace();
 		}
 		return true;
-	}
-	
-	@EventHandler
-	public void onPlayerJoinEvent(PlayerJoinEvent e) {
-		try {
-			if (config.getBoolean("options.force-gamemode.enable")) {
-				if (e.getPlayer().hasPermission("options.force-gamemode")) {
-					e.getPlayer().setGameMode(CGM.getGamemodeByIdOrName(config.get("options.force-gamemode.mode")));
-				} else
-					log("Didn't change " + e.getPlayer().getName() + "'s gamemode");
-			}
-		} catch(GameModeNotFoundException gme) {
-			log("Error: please check your config at 'options.force-gamemode.mode'", MessageColor.ERROR);
-			gme.printStackTrace();
-		}
-		otgm.put(e.getPlayer(), new Boolean[]{false, false, false, false}); // add the player to the change-gm-one-time-map
-		log("Player " + e.getPlayer().getName() + " has gamemode " + e.getPlayer().getGameMode().toString().toUpperCase());
-		if (e.getPlayer().getName() == "MCMainiac") broadcast("Hey MCMainiac! Author of GameModeControl!"); // this is our little secret :)
-	}
-	
-	//--------------
-	// Commands
-	//--------------
-	private boolean Gamemode(CommandSender sender, String[] args)
-			throws InvalidParameterException, GameModeNotFoundException {
-		ControlledGameMode cgm;
-		try {
-			cgm = CGM.getCGMByIdOrName(args[0]);
-		} catch (GameModeNotFoundException gme) {
-			send(sender, config.getString("Other.gamemode not found"), ImmutableMap.<String, String>builder().put("$gm", args[0].toUpperCase()).build());
-			return false;
-		}
-
-		if (sender instanceof Player)
-			if (args[1] == null) // check if there is a player specified
-				if (sender.hasPermission(permissions.get(args[args.length-1] + ".self")) || // Either you have permission to set your own game mode
-						sender.hasPermission(permissions.get(args[args.length-1])) || // or you have the global permission to set this game mode.
-						otgm.get(sender)[cgm.getId()]) { // or the player has been allowed to change his/her game mode via the /gmonce command
-					CGM.set((Player)sender, cgm);
-					otgm.put((Player) sender, new Boolean[]{false, false, false, false}); // reset the one-time-gm-map for this player
-				}
-				else
-					send((Player)sender, config.getString("Other.no permission")); // Oops you don't have permission to do that
-			else
-				try {
-					if (sender.hasPermission(permissions.get(args[args.length-1] + ".others")) || // the same over here, except that you 
-						sender.hasPermission(permissions.get(args[args.length-1])))
-							CGM.set(getPlayerByName(args[1]), (Player)sender, cgm);
-					else
-						send(sender, config.getString("Other.no permission"));
-				} catch (PlayerNotFoundException e) {
-					send(sender, config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[1]).build());
-				}
-		else
-			if (args[1] == null) return false; // we know it's the console or a command block here, so there MUST be a player specified
-			else
-				try {
-					CGM.set(getPlayerByName(args[1]), sender, cgm); // so just change the game mode (console has god mode activated)
-				} catch (PlayerNotFoundException e) {
-					send(sender, config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[1]).build());
-				}
-		return true;
-	}
-	
-	private boolean Reload(CommandSender sender) {
-		if (!sender.hasPermission(permissions.get("gmr"))) { send(sender, config.getString("Other.no permission")); return true; }
-		config.reload();
-		
-		// notify new settings
-		if (config.getBoolean("options.auto-update")) {
-			log("Auto-update §aenabled§7!");
-			checkForUpdates(true);
-			log("---------");
-		}
-		
-		if (config.getBoolean("options.force-gamemode.enable")) {
-			try {
-				ControlledGameMode cgm = CGM.getCGMByIdOrName(config.getString("options.force-gamemode.mode"));
-				this.getServer().setDefaultGameMode(cgm.getGamemode());
-				log("Forcing gamemode " + cgm.getName() + " on player join");
-			} catch (InvalidParameterException | GameModeNotFoundException e) {
-				log("You specified a wrong parameter for 'options.force-gamemode.mode'!", MessageColor.ERROR);
-				log("Using the default gamemode " + CGM.getMessageColor(ControlledGameMode.SURVIVAL) + "SURVIVAL", MessageColor.ERROR);
-				config.set("options.force-gamemode.mode", 0);
-				this.saveConfig();
-				this.reloadConfig();
-			}
-			log("---------");
-		}
-		
-		if (sender instanceof Player) log("Config reloaded");
-		sender.sendMessage(pre + "Config reloaded");
-		return true;
-	}
-	
-	private boolean Info(CommandSender sender, String[] args) {
-		if (!sender.hasPermission(permissions.get("gmi"))) { send(sender, config.getString("Other.no permission")); return true; }
-		sender.sendMessage("§f----- §7[§2Game§aMode§fControl§7] §f-----");
-		sender.sendMessage("§aVersion§7: §f" + this.getDescription().getVersion());
-		sender.sendMessage("§aAuthor§7: §fMCMainiac");
-		sender.sendMessage("§aWebsite§7: §5§nhttp://bit.ly/MC-GMC");
-		sender.sendMessage("§f-----------------------------");
-		return true;
-	}
-	
-	private boolean Help(CommandSender sender, String[] args) {
-		if (!sender.hasPermission(permissions.get("gmh"))) { send(sender, config.getString("Other.no permission")); return true; }
-		if (args.length == 0)
-			Help.Show(sender, 1);
-		else if (args.length == 1)
-			try {
-				Help.Show(sender, Integer.valueOf(args[0]));
-			} catch (NumberFormatException e) {
-				Help.Command(sender, args[0]);
-			}
-		else
-			return false;
-		return true;
-	}
-	
-	private boolean OneTimeGamemode(CommandSender sender, String[] args) {
-		if (!sender.hasPermission(permissions.get("gmonce"))) { send(sender, config.getString("Other.no permission")); return true; }
-		if (args.length > 0) {
-			try {
-				boolean survival = (args.length == 1 ? true : false), 
-						creative = (args.length == 1 ? true : false), 
-						adventure = (args.length == 1 ? true : false), 
-						spectator = (args.length == 1 ? true : false); // default for all game modes is false
-				
-				for (int i = 1; i < args.length; i++) { // go through all parameters and check, which game modes have been enabled by the operator
-					if (args[i].equalsIgnoreCase("survival")) survival = true;
-					if (args[i].equalsIgnoreCase("creative")) creative = true;
-					if (args[i].equalsIgnoreCase("adventure")) adventure = true;
-					if (args[i].equalsIgnoreCase("spectator")) spectator = true;
-				}
-				
-				otgm.put(getPlayerByName(args[0]), new Boolean[]{survival, creative, adventure, spectator});
-				sender.sendMessage("§6" + args[0] + " §fhas the permission to change his/her game mode to:");
-				String message = "";
-				for (ControlledGameMode cgm : ControlledGameMode.values())
-					if (otgm.get(getPlayerByName(args[0]))[cgm.getId()])
-						message += cgm.getMessageColor() + cgm.getName() + "§r, ";
-				
-				sender.sendMessage(message.substring(0, (message.length() - 4 > 0 ? message.length() - 4 : message.length())));
-			} catch (PlayerNotFoundException e) {
-				send(sender, config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[0]).build());
-			}
-			return true;
-		} else
-			return false;
 	}
 	
 	//--------------
@@ -394,6 +239,10 @@ public class Main extends JavaPlugin implements Listener {
 	public static void broadcast(String message) {
 		message = replaceColorCodes(message);
 		Bukkit.broadcastMessage(pre + (message != null ? message : "null"));
+	}
+
+	public static void broadcast(int i) {
+		Main.broadcast(Integer.toString(i));
 	}
 
 	public static String replaceColorCodes(String message) {
---
+++ src/com/mcmainiac/gmc/helpers/Commands.java
@@ -0,0 +1,211 @@
+package com.mcmainiac.gmc.helpers;
+
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import com.google.common.collect.ImmutableMap;
+import com.mcmainiac.gmc.Main;
+import com.mcmainiac.gmc.excpetions.GameModeNotFoundException;
+import com.mcmainiac.gmc.excpetions.PlayerNotFoundException;
+import com.mcmainiac.gmc.utils.CGM;
+import com.mcmainiac.gmc.utils.CGM.ControlledGameMode;
+import com.mcmainiac.gmc.utils.MessageColor;
+
+public class Commands implements Listener {
+	private static HashMap<Player, Boolean[]> otgm = new HashMap<Player, Boolean[]>(); // A map to save, which players are able to change their game mode
+	private static Main plugin;
+	
+	public Commands(Main main) {
+		Commands.plugin = main;
+
+		for (Player p : Bukkit.getOnlinePlayers()) { // if the server has been reloaded, this loop adds all players to the change-gm-one-time-map
+			otgm.put(p, new Boolean[]{false, false, false, false}); // default is false for every game mode
+		}
+	}
+
+	@EventHandler
+	public void onPlayerJoinEvent(PlayerJoinEvent e) {
+		try {
+			if (Main.config.getBoolean("options.force-gamemode.enable")) {
+				if (e.getPlayer().hasPermission("options.force-gamemode")) {
+					e.getPlayer().setGameMode(CGM.getGamemodeByIdOrName(Main.config.get("options.force-gamemode.mode")));
+				} else
+					Main.log("Didn't change " + e.getPlayer().getName() + "'s gamemode");
+			}
+		} catch(GameModeNotFoundException gme) {
+			Main.log("Error: please check your config at 'options.force-gamemode.mode'", MessageColor.ERROR);
+			gme.printStackTrace();
+		}
+		otgm.put(e.getPlayer(), new Boolean[]{false, false, false, false}); // add the player to the change-gm-one-time-map
+		Main.log("Player " + e.getPlayer().getName() + " has gamemode " + e.getPlayer().getGameMode().toString().toUpperCase());
+	}
+	
+	//--------------
+	// Commands
+	//--------------
+	public static boolean Gamemode(CommandSender sender, String[] args)
+			throws InvalidParameterException, GameModeNotFoundException {
+		ControlledGameMode cgm;
+		try {
+			cgm = CGM.getCGMByIdOrName(args[0]);
+		} catch (GameModeNotFoundException gme) {
+			Main.send(sender, Main.config.getString("Other.gamemode not found"), ImmutableMap.<String, String>builder().put("$gm", args[0].toUpperCase()).build());
+			return false;
+		}
+
+		if (sender instanceof Player)
+			if (args[1] == null) // check if there is a player specified
+				if (sender.hasPermission(Main.permissions.get(args[args.length-1] + ".self")) || // Either you have permission to set your own game mode
+						sender.hasPermission(Main.permissions.get(args[args.length-1])) || // or you have the global permission to set this game mode.
+						otgm.get(sender)[cgm.getId()]) { // or the player has been allowed to change his/her game mode via the /gmonce command
+					CGM.set((Player)sender, cgm);
+					otgm.put((Player) sender, new Boolean[]{false, false, false, false}); // reset the one-time-gm-map for this player
+				}
+				else
+					Main.send((Player)sender, Main.config.getString("Other.no permission")); // Oops you don't have permission to do that
+			else
+				try {
+					if (sender.hasPermission(Main.permissions.get(args[args.length-1] + ".others")) || // the same over here, except that you 
+						sender.hasPermission(Main.permissions.get(args[args.length-1])))
+							CGM.set(Main.getPlayerByName(args[1]), (Player)sender, cgm);
+					else
+						Main.send(sender, Main.config.getString("Other.no permission"));
+				} catch (PlayerNotFoundException e) {
+					Main.send(sender, Main.config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[1]).build());
+				}
+		else
+			if (args[1] == null) return false; // we know it's the console or a command block here, so there MUST be a player specified
+			else
+				try {
+					CGM.set(Main.getPlayerByName(args[1]), sender, cgm); // so just change the game mode (console has god mode activated)
+				} catch (PlayerNotFoundException e) {
+					Main.send(sender, Main.config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[1]).build());
+				}
+		return true;
+	}
+	
+	public static boolean Reload(CommandSender sender) {
+		if (!sender.hasPermission(Main.permissions.get("gmr"))) { Main.send(sender, Main.config.getString("Other.no permission")); return true; }
+		Main.config.reload();
+		
+		// notify new settings
+		if (Main.config.getBoolean("options.auto-update")) {
+			Main.log("Auto-update enabled!");
+			plugin.checkForUpdates(true);
+			Main.log("---------");
+		}
+		
+		if (Main.config.getBoolean("options.force-gamemode.enable")) {
+			try {
+				ControlledGameMode cgm = CGM.getCGMByIdOrName(Main.config.getString("options.force-gamemode.mode"));
+				plugin.getServer().setDefaultGameMode(cgm.getGamemode());
+				Main.log("Forcing gamemode " + cgm.getName() + " on player join");
+			} catch (InvalidParameterException | GameModeNotFoundException e) {
+				Main.log("You specified a wrong parameter for 'options.force-gamemode.mode'!", MessageColor.ERROR);
+				Main.log("Using the default gamemode " + CGM.getMessageColor(ControlledGameMode.SURVIVAL) + "SURVIVAL", MessageColor.ERROR);
+				Main.config.set("options.force-gamemode.mode", 0);
+				plugin.saveConfig();
+				plugin.reloadConfig();
+			}
+			Main.log("---------");
+		}
+		
+		if (sender instanceof Player) Main.log("Config reloaded");
+		sender.sendMessage(Main.pre + "Config reloaded");
+		return true;
+	}
+	
+	public static boolean Info(CommandSender sender, String[] args) {
+		if (!sender.hasPermission(Main.permissions.get("gmi"))) { Main.send(sender, Main.config.getString("Other.no permission")); return true; }
+		sender.sendMessage("§f----- §7[§2Game§aMode§fControl§7] §f-----");
+		sender.sendMessage("§aVersion§7: §f" + plugin.getDescription().getVersion());
+		sender.sendMessage("§aAuthor§7: §fMCMainiac");
+		sender.sendMessage("§aWebsite§7: §5§nhttp://bit.ly/MC-GMC");
+		sender.sendMessage("§f-----------------------------");
+		return true;
+	}
+	
+	public static boolean Help(CommandSender sender, String[] args) {
+		if (!sender.hasPermission(Main.permissions.get("gmh"))) { Main.send(sender, Main.config.getString("Other.no permission")); return true; }
+		if (args.length == 0)
+			Help.Show(sender, 1);
+		else if (args.length == 1)
+			try {
+				Help.Show(sender, Integer.valueOf(args[0]));
+			} catch (NumberFormatException e) {
+				Help.Command(sender, args[0]);
+			}
+		else
+			return false;
+		return true;
+	}
+	
+	public static boolean OneTimeGamemode(CommandSender sender, String[] args) {
+		if (!sender.hasPermission(Main.permissions.get("gmonce"))) { Main.send(sender, Main.config.getString("Other.no permission")); return true; }
+		if (args.length > 0) {
+			try {
+				boolean survival = (args.length == 1 ? true : false), 
+						creative = (args.length == 1 ? true : false), 
+						adventure = (args.length == 1 ? true : false), 
+						spectator = (args.length == 1 ? true : false); // default for all game modes is false
+				
+				for (int i = 1; i < args.length; i++) { // go through all parameters and check, which game modes have been enabled by the operator
+					if (args[i].equalsIgnoreCase("survival")) survival = true;
+					if (args[i].equalsIgnoreCase("creative")) creative = true;
+					if (args[i].equalsIgnoreCase("adventure")) adventure = true;
+					if (args[i].equalsIgnoreCase("spectator")) spectator = true;
+				}
+				
+				otgm.put(Main.getPlayerByName(args[0]), new Boolean[]{survival, creative, adventure, spectator});
+				sender.sendMessage("§6" + args[0] + " §fhas the permission to change his/her game mode to:");
+				String message = "";
+				for (ControlledGameMode cgm : ControlledGameMode.values())
+					if (otgm.get(Main.getPlayerByName(args[0]))[cgm.getId()])
+						message += cgm.getMessageColor() + cgm.getName() + "§r, ";
+				
+				sender.sendMessage(message.substring(0, (message.length() - 4 > 0 ? message.length() - 4 : message.length())));
+			} catch (PlayerNotFoundException e) {
+				Main.send(sender, Main.config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[0]).build());
+			}
+			return true;
+		} else
+			return false;
+	}
+	
+	public static boolean TemporaryGamemode(CommandSender sender, String[] args) {
+		if (!sender.hasPermission(Main.permissions.get("gmtemp"))) { Main.send(sender, Main.config.getString("Other.no permission")); return true; }
+		if (args.length > 1) {
+			try {
+				Player p = Main.getPlayerByName(args[0]); // get all infos we need
+				GameMode oldgm = p.getGameMode();
+				ControlledGameMode cgm = CGM.getCGMByIdOrName(args[1]);
+				CGM.set(p, cgm);
+				
+				p.getServer().getScheduler().scheduleSyncDelayedTask(Commands.plugin, new Runnable() {
+					@Override
+					public void run() {
+						try {
+							CGM.set(p, CGM.getCGMByGamemode(oldgm)); // reset the old gamemode of the player
+						} catch (GameModeNotFoundException e) { /* Ignore */ }
+					}
+				}, Long.parseLong(args[2])*20); // convert seconds to ticks (20 ticks = 1 second, if the server is running at normal speed (no lags))
+				
+				sender.sendMessage("§6" + p.getName() + "§f's gamemode mode is now " + cgm.getMessageColor() + cgm.getName() + " §ffor §a" + args[2] + " §fseconds");
+			} catch (PlayerNotFoundException e) {
+				Main.send(sender, Main.config.getString("Other.player not found"), ImmutableMap.<String, String>builder().put("$player", args[0]).build());
+			} catch (GameModeNotFoundException e) {
+				Main.send(sender, Main.config.getString("Other.gamemode not found"), ImmutableMap.<String, String>builder().put("$gm", args[0].toUpperCase()).build());
+			}
+			return true;	
+		} else
+			return false;
+	}
+}
