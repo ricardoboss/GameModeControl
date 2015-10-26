c3d71a4df4510f80f5b471e224ab4889b88df7ce
Ricardo Boss <ricardo.boss@web.de>
2015-10-26 17:29:50 +0100

    updated version

--- src/com/mcmainiac/gmc/Main.java
+++ src/com/mcmainiac/gmc/Main.java
@@ -23,7 +23,7 @@ import com.mcmainiac.gmc.helpers.Updater;
 import com.mcmainiac.gmc.utils.MessageColor;
 
 /**
- * GameModeControl V1.3.5
+ * GameModeControl V1.3.6
  * 
  * Helps you and your admins to control
  * game modes faster and more accurate
